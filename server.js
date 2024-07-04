58f383dca605e5430019ce731d780ee492d886d0
kunareddy harshareddy <kunareddyharshareddy@gmail.com>
2024-07-04 18:08:47 +0530

    hi

--- server.js
+++ server.js
@@ -1,51 +1,34 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const app = express()
-const dotenv = require('dotenv')
-const cors = require('cors')
-const path = require('path')
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const cors = require('cors');
+const path = require('path');
 
+app.use(express.json());
+app.use(cors({ credentials: true }));
 
-dotenv.config()
-app.use(express.json())
+app.use('/api', require('./routes/designRoutes'));
+app.use('/api', require('./routes/authRoutes'));
 
-if (process.env.NODE_ENV === 'local') {
-    app.use(cors({
-        origin: 'http://localhost:5173',
-        credentials: true
-    }))
-} else {
-    app.use(cors({
-        credentials: true
-    }))
-}
-
-app.use('/api', require('./routes/designRoutes'))
-app.use('/api', require('./routes/authRoutes'))
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, "./frontend/dist")))
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, "./", "frontend", "dist", "index.html"))
-    })
-}
+app.use(express.static(path.join(__dirname, './frontend/dist')));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './', 'frontend', 'dist', 'index.html'));
+});
 
 const dbConnect = async () => {
     try {
-        if (process.env.NODE_ENV === 'local') {
-            await mongoose.connect(process.env.LOCAL_DB_URI)
-            console.log('Local database is connect....')
-        } else {
-            await mongoose.connect(process.env.MONGODB_URI)
-            console.log('production database is connect....')
-        }
+        await mongoose.connect('mongodb+srv://kunareddyharshareddy:Harsha%409920@cluster0.uzqg8tx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Production database is connected...');
     } catch (error) {
-        console.log('database connection failed')
+        console.error('Database connection failed:', error);
     }
-}
+};
 
-dbConnect()
+dbConnect();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 2300;
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}..`))
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
